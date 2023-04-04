671d54c59e49aba5d451aae1c0baefd4496b0d7e
null-version-nova <ethcoyle@gmail.com>
2023-04-03 20:53:41 -0500

    update

--- core/src/main/kotlin/org/nullversionnova/data/TileGroup2.kt
+++ core/src/main/kotlin/org/nullversionnova/data/TileGroup2.kt
@@ -1,58 +1,7 @@
 package org.nullversionnova.data
 
-data class TileGroup2(val cornerA: IntegerVector2, val cornerB: IntegerVector2, val identifier: Identifier) {
-    fun getGreaterOnAxis(axis: Int) : IntegerVector2 {
-        return if (cornerA.getAxisFromInt(axis) > cornerB.getAxisFromInt(axis)) {
-            cornerA
-        } else {
-            cornerB
-        }
-    }
-    fun getLesserOnAxis(axis: Int) : IntegerVector2 {
-        return if (cornerA.getAxisFromInt(axis) < cornerB.getAxisFromInt(axis)) {
-            cornerA
-        } else {
-            cornerB
-        }
-    }
-    override fun equals(other: Any?) : Boolean {
-        val group1 : Array<Int> = arrayOf(4)
-        val group2 : Array<Int> = arrayOf(4)
-        if (other !is TileGroup2) { return super.equals(other) }
-        if (cornerA.x < cornerB.x) {
-            group1[0] = cornerA.x
-            group1[1] = cornerB.x
-        } else {
-            group1[0] = cornerB.x
-            group1[1] = cornerA.x
-        }
-        if (other.cornerA.x < other.cornerB.x) {
-            group2[0] = cornerA.x
-            group2[1] = cornerB.x
-        } else {
-            group2[0] = cornerB.x
-            group2[1] = cornerA.x
-        }
-        if (cornerA.y < cornerB.y) {
-            group1[2] = cornerA.y
-            group1[3] = cornerB.y
-        } else {
-            group1[2] = cornerB.y
-            group1[3] = cornerA.y
-        }
-        if (other.cornerA.y < other.cornerB.y) {
-            group2[2] = cornerA.y
-            group2[3] = cornerB.y
-        } else {
-            group2[2] = cornerB.y
-            group2[3] = cornerA.y
-        }
-        return (group1.contentEquals(group2) && identifier == other.identifier)
-    }
-    override fun hashCode(): Int {
-        var result = cornerA.hashCode()
-        result = 31 * result + cornerB.hashCode()
-        result = 31 * result + identifier.hashCode()
-        return result
+data class TileGroup2(val location: IntegerVector2, val scale: IntegerVector2, val identifier: Identifier) {
+    fun checkInBoundsOnAxis(axis: Int, depth: Int) : Boolean {
+        return depth >= location.getAxisFromInt(axis) && depth <= location.getAxisFromInt(axis) + scale.getAxisFromInt(axis)
     }
 }
--- core/src/main/kotlin/org/nullversionnova/data/TileGroups2.kt
+++ core/src/main/kotlin/org/nullversionnova/data/TileGroups2.kt
@@ -1,29 +1,20 @@
 package org.nullversionnova.data
 
-import org.nullversionnova.server.WorldCell
-
 data class TileGroups2(val group: MutableList<TileGroup2>) {
     fun findAllInPlane(axis: Int, depth: Int) : MutableSet<TileGroup2> {
         val candidates = mutableSetOf<TileGroup2>()
-        for (j in group) {
-            if (depth >= j.getLesserOnAxis(axis).getAxisFromInt(axis) && depth <= j.getGreaterOnAxis(axis).getAxisFromInt(axis)) { candidates.add(j) }
+        for (i in group) {
+            if (i.checkInBoundsOnAxis(axis,depth)) {
+                candidates.add(i)
+            }
         }
         return candidates
     }
     fun findTileGroup(position: IntegerVector2) : TileGroup2 {
         val candidates = mutableSetOf<TileGroup2>()
         for (i in findAllInPlane(0, position.x)) { candidates.add(i) }
-        for (i in candidates) { if (position.y < i.getLesserOnAxis(1).y || position.y > i.getGreaterOnAxis(1).y) { candidates.remove(i) } }
+        for (i in candidates) { if (!i.checkInBoundsOnAxis(1,position.y)) { candidates.remove(i) } }
         return candidates.first()
-    }
-    fun checkAllInBounds(tileGroup: TileGroup2) : MutableSet<IntegerVector2> {
-        val set = mutableSetOf<IntegerVector2>()
-        for (i in 0 until WorldCell.CELL_SIZE_X) {
-            if (i > tileGroup.getLesserOnAxis(0).x && i < tileGroup.getGreaterOnAxis(0).x) {
-                for (j in 0 until WorldCell.CELL_SIZE_Y) { set.add(IntegerVector2(i,j)) }
-            }
-        }
-        return set
     }
     fun listAllTilesInGroup() : MutableSet<Identifier> {
         val allTiles = mutableSetOf<Identifier>()
