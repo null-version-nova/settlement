52ac05a70e3789985b3d0e206797d8b637c1411b
null-version-nova <ethcoyle@gmail.com>
2023-04-05 19:17:22 -0500

    updated input

--- core/src/main/kotlin/org/nullversionnova/data/Identifier.kt
+++ core/src/main/kotlin/org/nullversionnova/data/Identifier.kt
@@ -2,4 +2,6 @@ package org.nullversionnova.data
 
 import org.nullversionnova.server.base.Base
 
-data class Identifier(val pack: String = Base.pack_identifier, val name: String)
+data class Identifier(val pack: String = Base.pack_identifier, val name: String) {
+    constructor(name: String) : this(Base.pack_identifier,name)
+}
