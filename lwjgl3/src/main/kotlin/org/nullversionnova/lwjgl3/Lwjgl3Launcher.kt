1afad116aba2224596a71eace3128fa0a2ef0a9b
null-version_nova <ethcoyle@gmail.com>
2023-04-12 20:17:12 -0500

    application name update

--- lwjgl3/src/main/kotlin/org/nullversionnova/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/org/nullversionnova/lwjgl3/Lwjgl3Launcher.kt
@@ -9,7 +9,7 @@ import org.nullversionnova.client.Client
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
     Lwjgl3Application(Client(), Lwjgl3ApplicationConfiguration().apply {
-        setTitle("settlement-kotlin")
+        setTitle("settlement")
         setWindowedMode(640, 480)
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
