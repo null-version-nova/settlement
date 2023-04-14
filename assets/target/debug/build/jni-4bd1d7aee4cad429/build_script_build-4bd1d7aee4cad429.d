921bc36dda5e31cb0fc92c631bd10bbc372ceb78
null-version-nova <ethcoyle@gmail.com>
2023-04-14 12:46:36 -0500

    update

---
+++ assets/target/debug/build/jni-4bd1d7aee4cad429/build_script_build-4bd1d7aee4cad429.d
@@ -0,0 +1,5 @@
+C:\Users\TheAviary\source\git\settlement\assets/target\debug\build\jni-4bd1d7aee4cad429\build_script_build-4bd1d7aee4cad429.exe: C:\Users\TheAviary\.cargo\registry\src\github.com-1ecc6299db9ec823\jni-0.19.0\build.rs
+
+C:\Users\TheAviary\source\git\settlement\assets/target\debug\build\jni-4bd1d7aee4cad429\build_script_build-4bd1d7aee4cad429.d: C:\Users\TheAviary\.cargo\registry\src\github.com-1ecc6299db9ec823\jni-0.19.0\build.rs
+
+C:\Users\TheAviary\.cargo\registry\src\github.com-1ecc6299db9ec823\jni-0.19.0\build.rs:
---
+++ assets/target/debug/build/syn-b8dadb2a8a8566bc/build_script_build-b8dadb2a8a8566bc.d
@@ -0,0 +1,5 @@
+C:\Users\TheAviary\source\git\settlement\assets/target\debug\build\syn-b8dadb2a8a8566bc\build_script_build-b8dadb2a8a8566bc.exe: C:\Users\TheAviary\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.109\build.rs
+
+C:\Users\TheAviary\source\git\settlement\assets/target\debug\build\syn-b8dadb2a8a8566bc\build_script_build-b8dadb2a8a8566bc.d: C:\Users\TheAviary\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.109\build.rs
+
+C:\Users\TheAviary\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.109\build.rs:
